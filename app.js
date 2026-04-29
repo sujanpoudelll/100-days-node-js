6af85610672701b702f0e28f0899b1d84d78251f
sujanpoudelll <poudelsujan59@gmail.com>
2026-04-29 16:02:13 +0545

    Day 1 completed

---
+++ app.js
@@ -0,0 +1,14 @@
+// Day 1 - Basics
+
+let name = "Sujan";
+let age = 25;
+
+console.log("Hello, my name is " + name +".");
+console.log("I am " + age + " years old.");
+
+//simple function
+function greet(user){
+    return "Welcome " + user;
+}
+
+console.log(greet(name));
---
+++ challenge.js
@@ -0,0 +1,10 @@
+let name = "Sujan"
+let fav_food = "Momo"
+
+console.log("My name is "+name+ " and I love "+fav_food+".") 
+
+function sum(a,b){
+    return a+b;
+}
+result = sum(5,6);
+console.log("The sum is "+result+".");
