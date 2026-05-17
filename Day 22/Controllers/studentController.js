2ec820d873ccdb413f77c39d9d3e73724157b3f9
sujanpoudelll <poudelsujan59@gmail.com>
2026-05-17 17:28:53 +0545

    Day 22: Added sorting, pagination, and improved GET API with advanced query handling

---
+++ Day 22/Controllers/studentController.js
@@ -0,0 +1,182 @@
+// Import Node.js File System module to perform CRUD operations on files
+const fs = require('fs'); 
+// Extracting specific function present in other file that is imported
+const {validateStudent} = require('../Utilities/validateStudent'); 
+
+// Reads the JSON file and parses the raw string data into usable JavaScript objects/arrays
+function getStudents(){
+    return JSON.parse(fs.readFileSync('Data/students.json','utf-8'));
+};
+
+// Converts the live JavaScript data back into a formatted JSON string and overwrites the file
+function saveStudents(data){
+    fs.writeFileSync('Data/students.json', JSON.stringify(data,null,2)); 
+};
+
+//http GET request 
+//route handler function is assigned to constant , req is incoming req from user and res is response from server to user
+const getAllStudents = (req, res) =>{
+    // logics 
+
+    let students = getStudents();
+    const {name, minmarks, sort, page, limit} = req.query;
+    
+    //Filter by minm marks
+    if (minmarks){
+        students = students.filter(student => student.marks >= Number(minmarks));
+    } 
+
+    //Filter by name
+    if(name){
+        students = students.filter(student => student.name.toLowerCase().includes(name.toLowerCase()));
+    }
+
+    //Sorting
+    if(sort === "marks"){
+        students.sort((a,b) => a.marks - b.marks);
+    }
+
+    if(sort === "name"){
+       students.sort((a,b) => a.name.localeCompare(b.name));
+    }
+
+    //Pagination
+    const pageNumber = Number(page) || 1;
+    const limitNumber = Number(limit) || 5;
+
+    const startIndex = (pageNumber - 1) * limitNumber;
+    const endIndex = pageNumber * limitNumber;
+
+    const paginatedStudents = students.slice(startIndex,endIndex);
+
+
+    //No students found
+    if (paginatedStudents.length === 0){
+        return res.status(404).json({
+        success: true,
+        message: "No data for this page !",
+        data: []
+    });
+    };
+    //Final response
+    res.status(200).json({
+        success: true,
+        count: paginatedStudents.length,
+        page: pageNumber,
+        limit: limitNumber,
+        data: paginatedStudents
+    });   
+    };
+
+
+//http POST request
+const addStudent = (req, res) => {
+
+    //logics with validation
+    const error = validateStudent(req.body); // function called with req.body as argument
+    if (error) {
+        return res.status(400).json({
+        success: false,
+        message: error
+    });
+}
+    const {name, marks} = req.body;
+    const students = getStudents();
+    const newStudent = {
+        id:Date.now(),
+         name, 
+         marks
+        };
+    students.push(newStudent);
+
+    saveStudents(students);
+    res.status(201).json({
+        success: true,
+        message: "New student added successfully !",
+        data: newStudent
+    });
+};
+
+//http PUT request
+const updateStudent = (req, res) => {
+    const id = Number(req.params.id); // operation with specific requests 
+    const error = validateStudent(req.body);
+    if (error) {
+        return res.status(400).json({
+        success: false,
+        message: error
+    });
+}
+    const {name, marks} =req.body;
+    let students = getStudents();
+    let found = false;
+    students = students.map(student=> {
+        if(student.id === id){
+            found = true;
+            return {
+                  ...student, //takes out original object and unpack its properties
+                  ...req.body //takes the data sent by user and unpack it below original 
+                  // then new data overwrites the old ones
+                
+            };  
+        }
+        return student;
+    });
+        if(!found){
+            return res.status(404).json({
+                success: false,
+                message: "Student not found !"
+            });
+        };
+    saveStudents(students);
+    res.status(200).json({
+        success: true,
+        message: " Student updated successfully !",   
+    });
+};
+
+//http DELETE request
+const deleteStudent = (req, res) => {
+    const id = Number(req.params.id);
+    let students = getStudents();
+    const filtered = students.filter(student => student.id !== id )
+    if(students.length === filtered.length){
+        return res.status(404).json({
+            success: false,
+            message: "Student not found !"
+        });
+    }
+    saveStudents(filtered);
+    res.status(200).json({
+        success: true,
+        message: "Student deleted successfully !"
+    });   
+};
+
+//http GET request for specific data
+const getStudentById = (req, res) => {
+   const id = Number(req.params.id);
+   let students = getStudents();
+   let found = false;
+   let requiredStudent = students.find(student => student.id === id);
+
+   if(!requiredStudent){
+    return res.status(404).json({
+        success: false,
+        message: "Student not found !"
+    });
+};
+    res.status(200).json({
+                success: true,
+                message: "Student found",
+                data: requiredStudent
+            }); 
+};
+
+module.exports = {
+    getAllStudents,
+    addStudent,
+    updateStudent,
+    deleteStudent,
+    getStudentById
+};
