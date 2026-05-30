b72f8dc503e2a88e79324f1d41fdd5458e4782d3
sujanpoudelll <poudelsujan59@gmail.com>
2026-05-30 12:04:54 +0545

    Day 29: Implement environment variables with dotenv and remove hardcoded secrets

---
+++ Day 29/Controllers/authController.js
@@ -0,0 +1,84 @@
+const bcrypt = require('bcryptjs'); //A library used to securely hash and store passwords
+const jwt = require('jsonwebtoken'); //A library used to generate unique, encrypted digital tokens
+
+const User = require('../Model/userModel'); //importing the userModel schema 
+
+const registerUser = async(req, res, next) => { // User registraion handling function
+    try{
+        const {name, email, password, role} = req.body;
+
+        //Check if user exists 
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            const error = new Error('User already exists !');
+            error.statusCode = 400;
+            throw error;
+        }
+
+        //hashed password
+        const hashPassword = await bcrypt.hash(password,10); //password is hashed making highly secure, unique and unreadable
+
+        //Create user
+        const user = await User.create({  //user creation
+            name, email, password: hashPassword, role
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully !"
+
+        });
+    }
+    catch(error){
+        next(error);
+      }
+
+}
+
+const loginUser = async(req, res, next) => { // User login function
+    try{
+        const {email, password} = req.body;
+
+        //Check if user exists
+        const user = await User.findOne({email}); //looks for user with req email
+
+        if(!user){
+            const error = new Error("Invalid email or password!");
+            error.statusCode = 401;
+            throw error;
+
+        }
+
+        //Compare password
+        const isMatch = await bcrypt.compare(password, user.password); //compares stored and entered password in hash
+        if(!isMatch){
+            const error = new Error("Invalid email or password!");
+            error.statusCode = 401;
+            throw error;   
+        }
+
+        //Generate JWT token
+        const token = await jwt.sign( //construct and encrypt token taking 3 major arguments
+            {id: user.id, email: user.email, role: user.role}, //Payload for embedding public data
+            process.env.JWT_SECRET, //Secret key for encryption
+            {expiresIn: "1d"} //Config options for seting lifespan of token
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful !",
+            token
+
+        });
+    }
+    catch(error){
+        next(error);
+      }
+
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+};
---
+++ Day 29/Middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+
+const protect = (req, res, next) => {
+    try {
+
+        // 1. get token from header
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            const error = new Error("No token provided");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        // 2. format: Bearer TOKEN
+        const token = authHeader.split(" ")[1];
+
+        if (!token) {
+            const error = new Error("Invalid token format");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        // 3. verify token
+        const decoded = jwt.verify(token,  process.env.JWT_SECRET);
+
+        // 4. attach user to request
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = protect;
