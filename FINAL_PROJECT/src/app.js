84f3e0477af0566df169f179236bd0391188c007
sujanpoudelll <poudelsujan59@gmail.com>
2026-06-10 11:10:58 +0545

    Day 36: Add security layer (Helmet, CORS, Rate limiting)

--- FINAL_PROJECT/src/app.js
+++ FINAL_PROJECT/src/app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+app.use(helmet());
+
+app.use(cors({
+    origin: "*"
+}));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: "Too many requests, try again later"
+});
+app.use(limiter);
+
 app.use(express.json());
+
+
 
 // routes
 const studentRoutes = require('./routes/studentRoutes');
