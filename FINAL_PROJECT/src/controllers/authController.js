dc5ce788d2e60e00d05bc024de25948b78d6a5cb
sujanpoudelll <poudelsujan59@gmail.com>
2026-06-12 14:54:33 +0545

    Day 41: Implement service layer architecture for auth system

--- FINAL_PROJECT/src/controllers/authController.js
+++ FINAL_PROJECT/src/controllers/authController.js
@@ -1,38 +1,18 @@
-const bcrypt = require('bcryptjs'); //A library used to securely hash and store passwords
-const jwt = require('jsonwebtoken'); //A library used to generate unique, encrypted digital tokens
-const AppError = require('../utils/appError');
-const config = require('../config/config');
-
-const User = require('../model/userModel'); //importing the userModel schema 
-
+const AppError = require('../utils/AppError');
 const {registerSchema,loginSchema} = require('../validators/authValidator');
 const asyncHandler = require('../utils/asyncHandler');
+const authService = require('../services/authService');
 
 const registerUser = asyncHandler(async(req, res, next) => { // User registraion handling function
 
-        const {name, email, password, role} = req.body;
         const result = registerSchema.safeParse(req.body);
         if(!result.success){
             const messages = result.error.issues.map(i => i.message).join(", ");
             return next(new AppError(messages, 400));
             
         }
-        
-        //Check if user exists 
-        const existingUser = await User.findOne({email});
 
-        if(existingUser){
-            throw new AppError("Email already exists",400);
-        }
-
-        //hashed password
-        const hashPassword = await bcrypt.hash(password,10); //password is hashed making highly secure, unique and unreadable
-
-        //Create user
-        const user = await User.create({  //user creation
-            name, email, password: hashPassword, role
-        });
-
+        await authService.register(req.body);
         res.status(201).json({
             success: true,
             message: "User registered successfully !"
@@ -43,35 +23,14 @@ const registerUser = asyncHandler(async(req, res, next) => { // User registraion
 
 const loginUser = asyncHandler(async(req, res, next) => { // User login function
     
-        const {email, password} = req.body;
+        
         const result = loginSchema.safeParse(req.body);
        if(!result.success){
             const messages = result.error.issues.map(i => i.message).join(", ");
             throw new AppError(messages,400)
 
         }
-
-        //Check if user exists
-        const user = await User.findOne({email}); //looks for user with req email
-
-        if(!user){
-            throw new AppError("Invalid email or password",401);
-
-        }
-
-        //Compare password
-        const isMatch = await bcrypt.compare(password, user.password); //compares stored and entered password in hash
-        if(!isMatch){
-            throw new AppError("Invalid email or password",401);
-            
-        }
-
-        //Generate JWT token
-        const token = await jwt.sign( //construct and encrypt token taking 3 major arguments
-            {id: user.id, email: user.email, role: user.role}, //Payload for embedding public data
-            config.jwtSecret, //Secret key for encryption
-            {expiresIn: "1d"} //Config options for seting lifespan of token
-        );
+        const token = await authService.login(req.body);
 
         res.status(200).json({
             success: true,
---
+++ FINAL_PROJECT/src/services/authService.js
@@ -0,0 +1,59 @@
+const User = require('../model/userModel');
+const bcrypt = require('bcryptjs');
+const AppError = require('../utils/AppError');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+ 
+
+//Register
+ const register = async(data) =>{
+ 
+    const {name, email, password, role} =data;
+       
+        //Check if user exists 
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            throw new AppError("Email already exists",400);
+        }
+
+        //hashed password
+        const hashPassword = await bcrypt.hash(password,10); //password is hashed making highly secure, unique and unreadable
+
+        //Create user
+        const user = await User.create({  //user creation
+            name, email, password: hashPassword, role
+        });
+
+        return user;
+    };
+
+    const login = async(data) =>{
+
+        const {email, password} = req.body;
+
+        //Check if user exists
+        const user = await User.findOne({email}); //looks for user with req email
+
+        if(!user){
+            throw new AppError("Invalid email or password",401);
+
+        }
+
+        //Compare password
+        const isMatch = await bcrypt.compare(password, user.password); //compares stored and entered password in hash
+        if(!isMatch){
+            throw new AppError("Invalid email or password",401);
+            
+        }
+
+        //Generate JWT token
+        const token = await jwt.sign( //construct and encrypt token taking 3 major arguments
+            {id: user.id, email: user.email, role: user.role}, //Payload for embedding public data
+            config.jwtSecret, //Secret key for encryption
+            {expiresIn: "1d"} //Config options for seting lifespan of token
+        );
+        return token
+    }
+
+    module.exports = {register,login};
